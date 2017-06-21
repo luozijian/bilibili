7f6f14bf6c38a378f9b6340d369f76d14be29c63
luozijian <lzj564774252@gmail.com>
2017-06-21 21:14:14 +0800

    feat:新增弹幕插件

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('style')
+    <link rel="stylesheet" type="text/css" href="/css/barrager.css">
     <style>
         .subtitle{
             position:absolute;
@@ -42,7 +43,21 @@
 
 @section('scripts')
     @parent
+    <script type="text/javascript" src="/js/jquery.barrager.js"></script>
+
     <script>
+        const item={
+            img:'/img/heisenberg.png', //图片
+            info:'弹幕文字信息', //文字
+            href:'http://www.yaseng.org', //链接
+            close:true, //显示关闭按钮
+            speed:8, //延迟,单位秒,默认8
+            bottom:70, //距离底部高度,单位px,默认随机
+            color:'#fff', //颜色,默认白色
+            old_ie_color:'#000000', //ie低版兼容色,不能与网页背景相同,默认黑色
+        };
+        $('body').barrager(item);
+
         $(function(){
             let chinese_subtitles = '{!! $chinese_subtitles !!}';
             let english_subtitles = '{!! $english_subtitles !!}';
